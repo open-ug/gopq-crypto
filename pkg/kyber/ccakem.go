d4b288dbb2ffd4c21b68b5f8268947a999eb1b5a
jim-junior <jimjunior854@gmail.com>
2024-10-20 11:36:39 +0300

    feat: Add CCAKEM and CPAPKE encryption and decryption functions to kyber package

--- pkg/kyber/ccakem.go
+++ pkg/kyber/ccakem.go
@@ -3,3 +3,16 @@ package kyber
 type CCAKEM struct {
 	// contains filtered or unexported fields
 }
+
+// KeyGen generates a public key and a secret key for the CCAKEM scheme.
+func (kem *CCAKEM) KeyGen() (publicKey, secretKey []byte) {
+	// ...
+}
+
+func (kem *CCAKEM) Encapsulate(publicKey []byte, coins []byte) (ciphertext, sharedSecret []byte) {
+	// ...
+}
+
+func (kem *CCAKEM) Decapsulate(ciphertext []byte, secretKey []byte) (sharedSecret []byte) {
+	// ...
+}
--- pkg/kyber/cpapke.go
+++ pkg/kyber/cpapke.go
@@ -3,3 +3,16 @@ package kyber
 type CPAPKE struct {
 	// contains filtered or unexported fields
 }
+
+// KeyGen generates a public key and a secret key for the CCAKEM scheme.
+func (kem *CPAPKE) KeyGen() (publicKey, secretKey []byte) {
+	// ...
+}
+
+func (kem *CPAPKE) Encrypt(publicKey []byte, message []byte, coins []byte) (ciphertext []byte) {
+	// ...
+}
+
+func (kem *CPAPKE) Decrypt(ciphertext []byte, secretKey []byte) (message []byte) {
+	// ...
+}
